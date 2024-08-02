3952502666fdc03476c556f82847b4cefee9d758
Abdullah <79790661+RhCodePi@users.noreply.github.com>
2024-08-03 01:07:06 +0300

    feat: add interfaces

---
+++ Todoapp.Auth.Service/src/@types/index.d.ts
@@ -0,0 +1,18 @@
+export interface User {
+  id: string;
+  email: string;
+  password: string;
+  name: string;
+  lastname: string;
+  todos?: Todo[];
+}
+
+export interface Todo {
+  id: number;
+  text: string;
+  author: string;
+  isCompleted: boolean;
+  expireDate: Date;
+  createdAt: Date;
+  updatedAt: Date;
+}
