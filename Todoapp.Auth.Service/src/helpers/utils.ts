0216d8fc3c1794446b0032105ce3f1c4a1d90f73
Abdullah <79790661+RhCodePi@users.noreply.github.com>
2024-08-03 01:26:37 +0300

    feat: add utils

---
+++ Todoapp.Auth.Service/src/helpers/utils.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express";
+import bcrypt from "bcrypt";
+
+export const validateFieldsForRequest = <T>(req: Request, res: Response, validateFields: string[]) => {
+
+    const missingFields: string[] = []
+    const object: Record<string, any> = {}
+
+    const keys = Object.keys(req.body)
+
+    for (let index = 0; index < validateFields.length; index++) {
+        const field = validateFields[index];    
+        
+        if(!keys.includes(field)){
+            missingFields.push(field)
+        }
+    }
+
+    if(missingFields.length > 0)
+    {
+        throw new Error(`fields are missing: ${missingFields.join(', ')}`);
+    }
+
+    validateFields.map((element) => {object[element]= req.body[element]})
+
+    return object as T
+}
+
+export const hashPassword = async (passowrd: string) =>{
+    const salt = await bcrypt.genSalt()
+    const hash = await bcrypt.hash(passowrd, salt)
+
+    return hash;
+}
