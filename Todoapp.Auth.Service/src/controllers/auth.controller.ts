f335136219f524a8e527a7eebce810d43d425c54
Abdullah <79790661+RhCodePi@users.noreply.github.com>
2024-08-03 01:22:19 +0300

    feat(auth-controller): implement sign-up

--- Todoapp.Auth.Service/src/controllers/auth.controller.ts
+++ Todoapp.Auth.Service/src/controllers/auth.controller.ts
@@ -1,10 +1,62 @@
 import { Request, Response, NextFunction } from "express";
+import { User } from "../@types";
+import { validateFieldsForRequest } from "../helpers/utils";
+
+import * as dotenv from "dotenv";
+import AuthService from "../service/auth.service";
+import { DocumentExistsError } from "couchbase";
+dotenv.config();
+
 
 export const signUp = async (
   req: Request,
   res: Response,
   next: NextFunction
-) => {};
+) => {
+  try {
+    if(req.headers.host !== process.env.HOST_SERVICE){
+      res.status(500).json({
+        success: false,
+        message: "Internal Server Error"
+      })
+      return;
+    }
+
+    const requiredFields = ["id", "email", "password", "name", "lastname"];
+
+    const validatedFields = validateFieldsForRequest<User>(
+      req,
+      res,
+      requiredFields
+    ) as User;
+
+    const createdUser = await AuthService.register(validatedFields)
+
+    res.status(201).json(
+      {
+        success: true,
+        data: {
+          createdUser
+        }
+      }
+    )
+
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(400).json({
+        success: false,
+        message: error.message,
+      });
+    }
+    else if(error instanceof DocumentExistsError)
+    {
+      res.status(400).json({
+        success: false,
+        message: "document already exists",
+      });
+    }
+  }
+};
 
 export const signIn = async (
   req: Request,
