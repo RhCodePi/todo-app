4eb4c3532f16ec40ea41d333f9680e34ea2a953d
Abdullah <79790661+RhCodePi@users.noreply.github.com>
2024-08-03 23:14:49 +0300

    feat(env): add environments for todo service

--- Todoapp.Gateway.Service/src/@types/env.d.ts
+++ Todoapp.Gateway.Service/src/@types/env.d.ts
@@ -1,13 +1,16 @@
-declare global{
-    namespace NodeJS{
-        interface ProcessEnv{
-            GATEWAY_SERVICE_PORT: string
-            PATHFILTER_REGEX: string
-            AUTH_PATH: string
-            TARGET: string
-        }
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      GATEWAY_SERVICE_PORT: string;
+      PATHFILTER_REGEX: string;
+      AUTH_PATH: string;
+      TARGET: string;
+      SECRET_KEY: string;
+      TODO_TARGET: string;
+      TODO_PATHFILTER_REGEX: string;
+      TODO_PATH: string;
     }
+  }
 }
 
-
-export {}
+export {};
