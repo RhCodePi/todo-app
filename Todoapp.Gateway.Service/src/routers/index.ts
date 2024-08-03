9297ad7496ef7b57660ca02285479084262bc930
Abdullah <79790661+RhCodePi@users.noreply.github.com>
2024-08-03 23:17:26 +0300

    feat(index): add proxy for todo service

--- Todoapp.Gateway.Service/src/routers/index.ts
+++ Todoapp.Gateway.Service/src/routers/index.ts
@@ -1,22 +1,30 @@
 import { Router } from "express";
 import *as dotenv from "dotenv";
 import { createProxyMiddleware} from "http-proxy-middleware";
+import { validToken } from "../middlewares/auth.middleware";
 
 dotenv.config();
 
 const router = Router();
 
-const pathFilter = function (path: string) {
-  return path.match(process.env.PATHFILTER_REGEX)?.[0];
+const pathFilter = function (path: string, regex: string) {
+  return path.match(regex)?.[0];
 };
 
-const filterdPath = pathFilter(process.env.AUTH_PATH);
+const filterdPath = pathFilter(process.env.AUTH_PATH, process.env.PATHFILTER_REGEX);
+const todoFilteredPath = pathFilter(process.env.TODO_PATH, process.env.TODO_PATHFILTER_REGEX)
 
 const apiProxyAuth = createProxyMiddleware({
   target: process.env.TARGET,
   pathFilter: filterdPath,
 });
 
+const todoApiProxy = createProxyMiddleware({
+  target: process.env.TODO_TARGET,
+  pathFilter: todoFilteredPath
+})
+
 router.use(apiProxyAuth);
+router.use(validToken, todoApiProxy);
 
 export default router;
