4af84a0f484219c72fcbc288e9092981b0d42976
Abdullah <79790661+RhCodePi@users.noreply.github.com>
2024-08-03 19:00:35 +0300

    feat(auth-service): check email exsist

--- Todoapp.Auth.Service/src/service/auth.service.ts
+++ Todoapp.Auth.Service/src/service/auth.service.ts
@@ -3,20 +3,25 @@ import { getCouchbaseConnection } from "../db";
 import { hashPassword } from "../helpers/utils";
 
 const register = async (user: User) => {
+  const { users } = await getCouchbaseConnection();
 
-    const connection = await getCouchbaseConnection()
+  const isEmailExists = await users.exists(user.email);
 
-    const hashedPassword = hashPassword(user.password)
+  if (isEmailExists.exists) {
+    throw new Error("email already exists");
+  }
+
+  const hashedPassword = await hashPassword(user.password);
 
     const updatedUser = {
-        ...user, password: hashedPassword
-    }
+    ...user,
+    password: hashedPassword,
+  };
 
-    const result = await connection.users.insert(user.id, updatedUser)
+  const result = await users.insert(user.id, updatedUser);
 
-    return result.cas
-}
-
+  return result.cas;
+};
 
 const AuthService = {
     register
