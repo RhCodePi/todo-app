c755ceb38d89e8c683107cb46a52abe83a471603
Abdullah <79790661+RhCodePi@users.noreply.github.com>
2024-08-04 17:52:28 +0300

    feat(todo-service): implement deleteTodo

--- Todoapp.Todo.Service/src/services/todo.service.ts
+++ Todoapp.Todo.Service/src/services/todo.service.ts
@@ -1,4 +1,5 @@
 import { Todo, User } from "../@types";
+import { getCouchbaseConnection } from "../db";
 import UserService from "./user.service";
 
 const createTodo = async (
@@ -14,8 +15,18 @@ const createTodo = async (
     };
   }
 
+  let id: number = 0;
+  const userMaxId = getUserTodoIdMax(user);
+  const deletedUserMaxId = await getDeletedUserMaxId(userId);
+
+  if (userMaxId > deletedUserMaxId) {
+    id = userMaxId;
+  } else if (deletedUserMaxId > userMaxId) {
+    id = deletedUserMaxId;
+  }
+
   const todo: Todo = {
-    id: (user.todos?.length as number) + 1,
+    id: id + 1,
     author: user.name + " " + user.lastname,
     createdAt: new Date(),
     updatedAt: new Date(),
@@ -30,12 +41,107 @@ const createTodo = async (
 
   return {
     cas: result,
-    todo
+    todo,
   };
+};
+
+const deleteTodo = async (userId: string, todoId: number) => {
+  let deletedUser: any;
+
+  const user: User = await UserService.getById(userId);
+
+  if (!user) throw new Error("user not found");
+
+  const todo = user.todos?.find((element) => {
+    if (element.id === todoId) {
+      return element;
+    }
+  });
+
+  if (!todo) throw new Error("todo not exists");
+
+  const index = user.todos?.indexOf(todo, 0) as number;
+  if (index > -1) {
+    user.todos?.splice(index, 1);
+  }
+
+  await UserService.updateUser(user);
+
+  const { deletedTodos, cluster } = await getCouchbaseConnection();
+
+  const queryResult = await cluster.query(
+    "SELECT todos FROM `todo-bucket`.`_default`.`deletedTodo` where id= $userId;",
+    {
+      parameters: {
+        userId,
+      },
+    }
+  );
+
+  let array: any = queryResult.rows.map((element) => element.todos);
+
+  deletedUser = {
+    ...user,
+    todos: array.reduce(
+      (acc: Todo[], current: Todo) => acc.concat(current),
+      []
+    ),
+  };
+
+  deletedUser.todos.push(todo);
+
+  delete deletedUser.password;
+
+  const result = await deletedTodos.upsert(deletedUser.id, deletedUser);
+
+  return {
+    cas: result.cas,
+    data: deletedUser,
+  };
+};
+
+const getUserTodoIdMax = (user: User) => {
+  const todo = user.todos as Todo[];
+
+  let maxId = 0;
+
+  if (todo.length === 0) return 0;
+
+  todo.map((todo) => {
+    if (todo.id > maxId) {
+      maxId = todo.id;
+    }
+  });
+
+  return maxId;
+};
+
+const getDeletedUserMaxId = async (userId: string) => {
+  const { cluster } = await getCouchbaseConnection();
+  let maxId = 0;
+  const queryResult = await cluster.query(
+    "SELECT todos FROM `todo-bucket`.`_default`.`deletedTodo` where id= $userId;",
+    {
+      parameters: {
+        userId,
+      },
+    }
+  );
+
+  if (queryResult.rows.length === 0) return 0;
+
+  let todos = queryResult.rows.map((element) => element.todos)[0];
+
+  todos.map((todo: Todo) => {
+    if (todo.id > maxId) maxId = todo.id;
+  });
+
+  return maxId;
 };
 
 const TodoService = {
   createTodo,
+  deleteTodo,
 };
 
 export default TodoService;
