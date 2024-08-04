970cbacb83d66992827ce2287c5d6929a491d9f2
Abdullah <79790661+RhCodePi@users.noreply.github.com>
2024-08-04 18:23:04 +0300

    feat(todo-router): add getTodo endpoint

--- Todoapp.Todo.Service/src/routers/todo.router.ts
+++ Todoapp.Todo.Service/src/routers/todo.router.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createTodo, deleteTodo } from "../controllers/todo.controller";
+import { createTodo, deleteTodo, getTodo } from "../controllers/todo.controller";
 
 const todoRouter = Router()
 
 todoRouter.post("/", createTodo)
 todoRouter.delete("/:id", deleteTodo)
+todoRouter.get("/:id", getTodo)
 
 export default todoRouter;
