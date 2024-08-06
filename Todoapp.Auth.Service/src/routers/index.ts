7d7fd0767057c5b734bdbd8b9575619a1d80ff9c
Abdullah <79790661+RhCodePi@users.noreply.github.com>
2024-08-06 16:02:33 +0300

    feat(index): add middleware to route

--- Todoapp.Auth.Service/src/routers/index.ts
+++ Todoapp.Auth.Service/src/routers/index.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import authRouter from "./auth.router";
+import { hostMiddleware } from "../middlewares/host.middleware";
 
 const router = Router();
 
-router.use("/auth", authRouter)
+router.use("/auth", hostMiddleware, authRouter)
 
 export default router;
